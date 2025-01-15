1aa688d7c2cbd46e1b10e861bc41d6cfa72c6bc8
Charley <charley.geoffroy@protonmail.com>
2025-01-15 18:18:42 +0100

    chore: add peer list from yaml file

--- src/layout/header.rs
+++ src/layout/header.rs
@@ -1,24 +1,25 @@
 use crate::app::ChatApp;
 use crate::message::ContextView;
+use crate::peer_config::PeerConfig;
 use eframe::egui;
 use egui::TextEdit;
 
 pub struct HeaderLayout {
-    // pub peers: Vec<String>,
+    pub peers: Vec<String>,
     pub peer_endpoint: String,
 }
 
 impl HeaderLayout {
     pub fn new(app: &ChatApp) -> Self {
-        // let peer_config = PeerConfig::load_from_file("peer-config.yaml");
-        // let peers = peer_config
-        //     .peer_list
-        //     .iter()
-        //     .map(|peer| peer.endpoint.clone())
-        //     .collect();
+        let peer_config = PeerConfig::load_from_file("peer-config.yaml");
+        let peers = peer_config
+            .peer_list
+            .iter()
+            .map(|peer| peer.endpoint.clone())
+            .collect();
         Self {
             peer_endpoint: app.peer_endpoint.clone(),
-            // peers,
+            peers,
         }
     }
 
@@ -40,21 +41,16 @@ impl HeaderLayout {
         ui.horizontal(|ui| {
             ui.label("Peer Endpoint:");
             ui.add_enabled(false, TextEdit::singleline(&mut self.peer_endpoint.clone()));
+            ui.label("Peer List:");
+            egui::ComboBox::from_id_salt("Peers")
+                .selected_text(format!("{:?}", self.peer_endpoint))
+                .show_ui(ui, |ui| {
+                    for peer in &self.peers {
+                        ui.selectable_value(&mut self.peer_endpoint, peer.clone(), peer);
+                    }
+                });
         });
 
-        // ui.separator();
-        // ui.add_space(10.0);
-        // Peer List
-        // ui.horizontal(|ui| {
-        //     ui.label("Peer List:");
-        //     egui::ComboBox::from_id_salt("Peers")
-        //         .selected_text(format!("{:?}", self.peer_endpoint))
-        //         .show_ui(ui, |ui| {
-        //             for peer in &self.peers {
-        //                 ui.selectable_value(&mut self.peer_endpoint, peer.clone(), peer);
-        //             }
-        //         });
-        // });
         ui.add_space(10.0);
     }
 }
