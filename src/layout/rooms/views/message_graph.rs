94fda13fe555bffba7d980cbb2eece246ee69dc2
Olivier De Jonckere <olivierdejonckere@gmail.com>
2025-06-25 22:52:21 +0200

    Revert "chore: bump + ui fix"
    This reverts commit 0bb278f433483898a8c2496c17bb5c1be0351805.

--- src/layout/rooms/views/message_graph.rs
+++ src/layout/rooms/views/message_graph.rs
@@ -110,7 +110,7 @@ impl MessageGraphView {
             })
             .auto_reset(reset_requested)
             .show(ui, |plot_ui| {
-                plot_ui.vline(VLine::new("Current Time", now).color(Color32::from_rgb(255, 0, 0)));
+                plot_ui.vline(VLine::new(now).name("Current Time").color(Color32::from_rgb(255, 0, 0)));
 
                 for (_uuid, (peer, boxes)) in per_sender {
                     let peer_name = peer.name.clone();
@@ -118,7 +118,7 @@ impl MessageGraphView {
                     // Create a new String that we can move into the closure
                     let formatter_peer_name = peer_name.clone();
 
-                    let box_for_senders = BoxPlot::new(peer_name.clone(), boxes)
+                    let box_for_senders = BoxPlot::new(boxes).name(peer_name.clone())
                         .color(peer.get_color())
                         .horizontal()
                         .allow_hover(true)
